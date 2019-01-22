f267ad51ac676487256c6df5be71cb9f7721ec76
Kyle Lee <Kylecrlee95@gmail.com>
2019-01-22 16:17:48 -0600

    peas and carrot

--- models.js
+++ models.js
@@ -1,26 +1,26 @@
 const Sequelize = require("sequelize");
 const db = new Sequelize("postgres://localhost:5432/plantr");
 
-const Gardener = db.define('gardener', {
+const Gardener = db.define("gardener", {
   name: Sequelize.STRING,
   age: Sequelize.INTEGER
-})
+});
 
-const Plot = db.define('plot', {
+const Plot = db.define("plot", {
   size: Sequelize.INTEGER,
   shaded: Sequelize.BOOLEAN
-})
+});
 
-const Vegetable = db.define('vegetable', {
+const Vegetable = db.define("vegetable", {
   name: Sequelize.STRING,
   color: Sequelize.STRING,
   planted_on: Sequelize.DATE
-})
+});
 
-Plot.belongsTo(Gardener)
+Plot.belongsTo(Gardener);
 // Gardener.belongsTo(Plot)
-Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'})
-Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'})
-Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'})
+Vegetable.belongsToMany(Plot, { through: "vegetable_plot" });
+Plot.belongsToMany(Vegetable, { through: "vegetable_plot" });
+Gardener.belongsTo(Vegetable, { as: "favorite_vegetable" });
 
-module.exports = { db };
+module.exports = { db, Gardener, Plot, Vegetable };
--- seed.js
+++ seed.js
@@ -1,8 +1,19 @@
-const { db } = require("./models");
+const { db, Gardener, Plot, Vegetable } = require("./models");
+
+const vegetableData = [
+  { name: "carrot", color: "orange", planted_on: new Date() },
+  { name: "peas", color: "green", planted_on: new Date() }
+];
 
 db.sync({ force: true })
   .then(() => {
     console.log("synced db");
+  })
+  .then(() => {
+    const promiseForVegetables = Vegetable.bulkCreate(vegetableData, {
+      returning: true
+    });
+    return Promise.all([promiseForVegetables]);
   })
   .catch(error => {
     console.log(error);

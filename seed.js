fda9a17e112df47c21001a3a70bc81f90d160b22
Kyle Lee <Kylecrlee95@gmail.com>
2019-01-22 15:44:40 -0600

    Working on Database Schema

---
+++ seed.js
@@ -0,0 +1,12 @@
+const { db } = require("models");
+
+db.sync({ force: true })
+  .then(() => {
+    console.log("synced db");
+  })
+  .catch(error => {
+    console.log(error);
+  })
+  .finally(() => {
+    db.close();
+  });
